7801762a55629c81769d18b57435d96777f1e045
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-01-16 11:27:10 +0900

    output change

--- output.tf
+++ output.tf
@@ -3,6 +3,6 @@ output "tfe-server-domain" {
   value = "https://${aws_eip.tfe-eip.public_dns}"
 }
 
-ouput "private_pem" {
+output "private_pem" {
   value = tls_private_key.tfe-priv-key.private_key_pem
 }

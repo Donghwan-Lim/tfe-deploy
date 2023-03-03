75dd4b66be42d3f3354c49f1e9bd468dd237f54d
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-03-03 15:30:18 +0900

    Create_ACM-ALB TFE deploy2

--- output.tf
+++ output.tf
@@ -1,8 +1,4 @@
 # Outputs file
-output "tfe-server-domain" {
-  value = "https://${aws_eip.tfe-eip.public_dns}"
-}
-
 output "private_pem" {
   value = tls_private_key.tfe-priv-key.private_key_pem
   sensitive = true

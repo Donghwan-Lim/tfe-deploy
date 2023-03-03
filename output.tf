293acdc7e63a0aa1519df36940ff21f99dd5eb8c
Donghwan-Lim <ldh0727@insideinfo.co.kr>
2023-03-03 15:57:53 +0900

    output_update

--- output.tf
+++ output.tf
@@ -3,3 +3,8 @@ output "private_pem" {
   value = tls_private_key.tfe-priv-key.private_key_pem
   sensitive = true
 }
+
+output "instance_dns" {
+  value = aws_instance.tfe-server.public_dns
+  sensitive = false
+}
